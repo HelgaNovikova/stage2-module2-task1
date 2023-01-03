c80c55374237511cbf3876a722787d8df3dc9673
HelgaNovikova <helpotaki@rambler.ru>
2023-01-03 15:22:02 -0500

    st2m2t1 t3

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -15,13 +15,9 @@ public class GetUsersServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String firstName = req.getParameter("firstName");
-        String lastName = req.getParameter("lastName");
-        User newUser = new User(firstName, lastName);
-        Warehouse.getInstance().addUser(newUser);
         resp.setContentType("text/html");
         ServletContext context=getServletContext();
-        context.setAttribute("user",newUser);
+        context.setAttribute("users",Warehouse.getInstance().getUsers());
         resp.sendRedirect(req.getContextPath() + "/users");
     }
     //write your code here!
