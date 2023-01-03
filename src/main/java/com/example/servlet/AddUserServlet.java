3be2b536d2d026858fb13f4e552b8ae68793b8d4
HelgaNovikova <helpotaki@rambler.ru>
2023-01-03 15:05:11 -0500

    st2m2t1 t1

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,23 @@
 package com.example.servlet;
 
-public class AddUserServlet {
+import com.example.Warehouse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
     //write your code here!
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        ServletContext context = getServletContext();
+        context.setAttribute("users", Warehouse.getInstance().getUsers());
+        resp.sendRedirect("/add");
+    }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,28 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        User newUser = new User(firstName, lastName);
+        Warehouse.getInstance().addUser(newUser);
+        resp.setContentType("text/html");
+        ServletContext context=getServletContext();
+        context.setAttribute("user",newUser);
+        resp.sendRedirect("/users");
+    }
     //write your code here!
 }
